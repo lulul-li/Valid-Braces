696eef1e6fa38f5be829eeed5d715a1baaa9a50e
lulu <s987337613@gmail.com>
2017-11-26 21:35:44 +0800

    extra vaild helper

--- VaildBracesTest/VaildBracesTest/VaildBracesTest.cs
+++ VaildBracesTest/VaildBracesTest/VaildBracesTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace VaildBracesTest
@@ -6,25 +7,13 @@ namespace VaildBracesTest
     [TestFixture]
     public class VaildBracesTest
     {
-        [TestCase("[",false,TestName = "input one char should return false")]
+        [TestCase("[", false, TestName = "input one char should return false")]
         [TestCase("[{", false, TestName = "input two char but not match braces should return false")]
         [TestCase("[{}]", true, TestName = "input  match braces [{}] should return true")]
         public void TestMethod1(string input, bool expected)
         {
-            var result = VaildBraces(input);
+            var result = new VaildHelper().VaildBraces(input);
             Assert.AreEqual(expected, result);
-        }
-
-        public bool VaildBraces(string input)
-        {
-            for (var i = 0; i < input.Length-1; i++)
-            {
-                if (input[i] == input[i + 1])
-                {
-                    return true;
-                }
-            }
-            return false;
         }
     }
 }
---
+++ VaildBracesTest/VaildBracesTest/VaildHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace VaildBracesTest
+{
+    public class VaildHelper
+    {
+        public bool VaildBraces(string input)
+        {
+            if (input.Length % 2 != 0)
+            {
+                return false;
+            }
+            var starIndex = 0;
+            var endIndex = input.Length - 1;
+            for (; starIndex < endIndex; starIndex++, endIndex--)
+            {
+                if (!CheckBraceIsMatch(input, starIndex, endIndex))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static bool CheckBraceIsMatch(string input, int starIndex, int endIndex)
+        {
+            var Brace = new Dictionary<char, char>
+            {
+                { '[',']'},
+                { '(',')'},
+                { '{','}'}
+            };
+            return Brace[input[starIndex]] == input[endIndex];
+        }
+    }
+}
