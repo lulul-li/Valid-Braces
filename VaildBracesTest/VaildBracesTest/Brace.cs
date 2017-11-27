f598db01d8e70ea89dae455c9f0fd668715e0d4a
lulu <s987337613@gmail.com>
2017-11-28 01:05:25 +0800

    change list to stack and add input field

---
+++ VaildBracesTest/VaildBracesTest/Brace.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace VaildBracesTest
+{
+    public class Brace
+    {
+        private static readonly Dictionary<char, char> BracketsDictionary = new Dictionary<char, char>
+        {
+            { '[',']'},
+            { '(',')'},
+            { '{','}'}
+        };
+
+        private static string _input;
+
+        public Brace(string input)
+        {
+            _input = input;
+        }
+
+
+        public bool VaildBraces()
+        {
+            if (LenIsNotVaild())
+            {
+                return false;
+            }
+
+            var bracketsStack = new Stack<char>();
+            for (var index = 0; index < _input.Length; index++)
+            {
+                AddFirstBracket(index, bracketsStack);
+                RemoveMatchBraces(index, bracketsStack);
+            }
+
+            return bracketsStack.Count == 0;
+        }
+
+        private static void AddFirstBracket(int i, Stack<char> stack)
+        {
+            if (BracketsDictionary.ContainsKey(_input[i]))
+            {
+                stack.Push(_input[i]);
+            }
+        }
+
+        private static void RemoveMatchBraces(int i, Stack<char> stack)
+        {
+            if (stack.Any() && BracketsDictionary[stack.Peek()] == _input[i])
+            {
+                stack.Pop();
+            }
+        }
+
+        private static bool LenIsNotVaild()
+        {
+            return _input.Length % 2 != 0;
+        }
+    }
+}
--- VaildBracesTest/VaildBracesTest/VaildBracesTest.cs
+++ VaildBracesTest/VaildBracesTest/VaildBracesTest.cs
@@ -11,9 +11,11 @@ namespace VaildBracesTest
         [TestCase("[{", false, TestName = "input two char but not match braces should return false")]
         [TestCase("[{}]", true, TestName = "input  match braces [{}] should return true")]
         [TestCase("[{}][]", true, TestName = "input  match braces [{}][] should return true")]
-        public void TestMethod1(string input, bool expected)
+        [TestCase(")(}{][", false, TestName = "input  )(}{][ should return false")]
+        [TestCase("())({}}{()][][", false, TestName = "input ())({}}{()][][ should return false")]
+        public void VaildBracesReturnIsCorrect(string input, bool expected)
         {
-            var result = new VaildHelper().VaildBraces(input);
+            var result = new Brace(input).VaildBraces();
             Assert.AreEqual(expected, result);
         }
     }
